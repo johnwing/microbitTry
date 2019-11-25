886e2441d61272b57d5d251fb3b03cf8da2ab7f5
johnwing <47427803+johnwing@users.noreply.github.com>
2019-11-26 05:21:41 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,8 @@ namespace tools {
     * 計算長方形面積，不回傳，只顯示在LED
     */
     //% shim=tools::obloqDisDisplay
+    //% blockId="obloqDisDisplay" block="area of rectangle"
+    //% blockGap=2 weight=0 blockExternalInputs=true
     function obloqDisDisplay(): void {
         return
     }
